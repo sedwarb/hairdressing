0073ff05c2df500b7aa5e8a138dcb7cfacb932f6
sedwarb <sedwarb@gmail.com>
2022-06-24 21:12:33 -0500

    added function createEntries

---
+++ api/src/controllers/entries.js
@@ -0,0 +1,55 @@
+const { User, Entry, Worker, Service } = require("../db.js");
+const { Op } = require("sequelize");
+
+async function getEntriesByDate(req, res){    
+    let entriesBydate = await Entry.findAll(
+        {//2021-03-01 year-month-day
+            where:{date:{[Op.between]:[req.query.dateIni,req.query.dateEnd]}},
+            attributes: [
+                "id", 
+                "date",
+                "manualEntry",
+                "amountEntry",
+                "entryType"],
+            include:[
+                {model: User,attributes:[
+                    "phoneNumber",
+                    "fullname"
+                ]},
+                {model: Worker,attributes:[
+                    "id",
+                    "fullname"
+                ]},
+                {model: Service,attributes:[
+                    "id",
+                    "name",
+                    "amount",
+                    "info"
+                ]}]
+        }
+    )
+    //entriesBydate[0].id
+    res.send(entriesBydate)
+}
+
+async function createEntry(req, res){
+    try{
+        await Entry.create(
+            {
+                date:Date(),
+                manualEntry:req.body.manualEntry,
+                amountEntry:req.body.amountEntry,
+                entryType:req.body.entryType,
+                userPhoneNumber:req.body.userPhoneNumber,
+                workerId:req.body.workerId,
+                serviceId:req.body.serviceId
+            }
+        )
+        res.send(`Entrada Creada Exitosamente`)
+    }catch(error){res.send(`Error: ${error}`)}
+}
+
+module.exports= {
+    getEntriesByDate,
+    createEntry
+}

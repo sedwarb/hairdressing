45656c8151662e35abb0b462b7346cca4b94e8c5
sedwarb <sedwarb@gmail.com>
2022-07-08 14:48:07 -0500

    added css from bootstrap

--- client/src/components/Lists/Lists.jsx
+++ client/src/components/Lists/Lists.jsx
@@ -9,11 +9,14 @@ export function Lists({typeL,fieldL,esp,listStP}){
         .catch(error=>console.log(`Error: ${error}`))
     },[typeL])
     return(
-        <>
-            <div>{`${esp}`}</div>
-            <select onChange={(e)=>listStP[`${typeL}Id`]=e.currentTarget.value}>
-                {lists && lists.map(list=><option value={list.id}>{list[fieldL]}</option>)}
-            </select>
+        <>            
+            <div className="col-sm-6" class="w-50 p-3">
+                <label className="form-label" >{`${esp}`}</label>
+                <select className="form-label" class="form-control" onChange={(e)=>listStP[`${typeL}Id`]=e.currentTarget.value}>
+                    {lists && lists.map(list=><option class="list-group-item" key={list.id} value={list.id}>{list[fieldL]}</option>)}
+                </select>
+            </div>
+            
         </>
     )
 }
--- client/src/components/entries/Entries.jsx
+++ client/src/components/entries/Entries.jsx
@@ -1,23 +1,58 @@
 import React,{useState} from 'react'
 import {Lists} from '../Lists/Lists'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 export function Entries(){
     const [listSt] = useState({workerId:"",serviceId:""})
+    const [value, setValue] = useState(new Date());
+    const [visible,setVisible] = useState(false);
+    function onChange(nextValue) {
+        setValue(nextValue);
+    }
     return(
         <>            
-            <div>Telefono del Usuario</div>
-            <input type='search'/>
-            <div>Fecha</div>
-            <input type='search'/>
-            <div><Lists typeL="worker" fieldL="fullname" esp="Trabajadores" listStP={listSt}/></div>
-            <div><Lists typeL="service" fieldL="name" esp="Servicios" listStP={listSt}/></div>
-            <div>Precio</div>
-            <input type='search'/>
-            <div>Entrada</div>
-            <select>
-                <option value="entry">Entrada Simple</option>
-                <option value="meeting">Cita</option>
-            </select>            
+            <div className="col-md-7 col-lg-8">
+                <h4 className="mb-3">Datos de la Entrada</h4>
+                <form className="needs-validation" noValidate>
+                    <div className="row g-3">
+                        <div className="col-sm-6" class="w-50 p-3">
+                            <label htmlFor="telephone" className="form-label">Numero de Telefono</label>
+                            <input type="text" className="form-control" id="telephone" placeholder="" required/>
+                            <div className="invalid-feedback">
+                                Se requiere un Numero de telefono Valido.
+                            </div>
+                        </div>
+                        <div className="col-sm-6" class="w-50 p-3">
+                            <label htmlFor="precio" className="form-label">Precio</label>
+                            <input type="text" className="form-control" id="precio" placeholder="" required/>
+                            <div className="invalid-feedback">
+                                Se requiere un precio valido.
+                            </div>
+                        </div>
+                        <div className="col-sm-6" class="w-100 p-3">
+                            <label className="form-label">Fecha</label>
+                            <label onClick={()=>visible?setVisible(false):setVisible(true)} className="form-label">{JSON.stringify(value)}</label>
+                            <div className="invalid-feedback">
+                                Se requiere una fecha valida.
+                            </div>
+                            <div></div>
+                            {visible?<Calendar onChange={onChange} value={value}/>:<div></div>}                            
+                        </div>                        
+                        <div className="col-sm-6" class="w-50 p-3">
+                            <label className="form-label" >Entrada</label>
+                            <select className="form-label" class="form-control">
+                                <option defaultValue="entry">Entrada Simple</option>
+                                <option defaultValue="meeting">Cita</option>
+                            </select>
+                        </div>
+                        <Lists typeL="worker" fieldL="fullname" 
+                        esp="Trabajadores" listStP={listSt}/>
+                        <Lists typeL="service" fieldL="name"
+                        esp="Servicios" listStP={listSt}/>
+                    </div>                    
+                </form>                
+            </div>
         </>
     )
 }

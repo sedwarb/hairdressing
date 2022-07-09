84b5f339531f5155bd209f7c0eeba20f1063ecb0
sedwarb <sedwarb@gmail.com>
2022-07-08 23:46:06 -0500

    added bootstrap and new function for create in database new entry

--- client/src/components/Lists/Lists.jsx
+++ client/src/components/Lists/Lists.jsx
@@ -12,8 +12,9 @@ export function Lists({typeL,fieldL,esp,listStP}){
         <>            
             <div className="col-sm-6" class="w-50 p-3">
                 <label className="form-label" >{`${esp}`}</label>
-                <select className="form-label" class="form-control" onChange={(e)=>listStP[`${typeL}Id`]=e.currentTarget.value}>
-                    {lists && lists.map(list=><option class="list-group-item" key={list.id} value={list.id}>{list[fieldL]}</option>)}
+                <select className="form-label form-control" onChange={(e)=>listStP[`${typeL}Id`]=e.currentTarget.value}>
+                    <option className="list-group-item">Seleccione...</option>
+                    {lists && lists.map(list=><option className="list-group-item" key={list.id} value={list.id}>{list[fieldL]}</option>)}
                 </select>
             </div>
             
--- client/src/components/entries/Entries.jsx
+++ client/src/components/entries/Entries.jsx
@@ -5,32 +5,51 @@ import 'react-calendar/dist/Calendar.css';
 
 export function Entries(){
     const [listSt] = useState({workerId:"",serviceId:""})
-    const [value, setValue] = useState(new Date());
+    const [value, setValue] = useState(new Date());    
     const [visible,setVisible] = useState(false);
+    const [entrada,setEntrada] = useState("entry");
+    const [stateGen,setStateGen] = useState({});
     function onChange(nextValue) {
         setValue(nextValue);
+    }
+    function handleChange(e){
+        setStateGen({...stateGen,[e.target.id]:e.target.value})
+      }
+    function onSubmit(e) {
+        e.preventDefault();
+        const options = {method: "POST",headers:{"Content-Type": "application/json"},
+        body: JSON.stringify(
+            {
+                entryType:entrada,
+                date:value,
+                serviceId:listSt.serviceId,
+                workerId:listSt.workerId,
+                userPhoneNumber:stateGen.telephone?stateGen.telephone:"3006007050",
+                manualEntry:listSt.serviceId==="inma"?stateGen.manual:"",
+                amountEntry:listSt.serviceId==="inma"?parseFloat(stateGen.precio):0
+            }
+        
+        )};
+        fetch(`http://localhost:3001/entries`,options)
+        .then(response => response.json())
+        .catch(error =>console.log(`Este fue el Error: ${error}`))
     }
     return(
         <>            
             <div className="col-md-7 col-lg-8">
                 <h4 className="mb-3">Datos de la Entrada</h4>
-                <form className="needs-validation" noValidate>
+                <form onSubmit={e=>onSubmit(e)} className="needs-validation" noValidate>
                     <div className="row g-3">
-                        <div className="col-sm-6" class="w-50 p-3">
+                        <div className="col-sm-6 w-50 p-3">
                             <label htmlFor="telephone" className="form-label">Numero de Telefono</label>
-                            <input type="text" className="form-control" id="telephone" placeholder="" required/>
+                            <input onChange={e=>handleChange(e)} id="telephone" type="text" className="form-control" placeholder="" required/>
                             <div className="invalid-feedback">
                                 Se requiere un Numero de telefono Valido.
                             </div>
                         </div>
-                        <div className="col-sm-6" class="w-50 p-3">
-                            <label htmlFor="precio" className="form-label">Precio</label>
-                            <input type="text" className="form-control" id="precio" placeholder="" required/>
-                            <div className="invalid-feedback">
-                                Se requiere un precio valido.
-                            </div>
-                        </div>
-                        <div className="col-sm-6" class="w-100 p-3">
+                        <Lists typeL="service" fieldL="name"
+                        esp="Servicios" listStP={listSt}/>                        
+                        <div className="col-sm-6 w-100 p-3" >
                             <label className="form-label">Fecha</label>
                             <label onClick={()=>visible?setVisible(false):setVisible(true)} className="form-label">{JSON.stringify(value)}</label>
                             <div className="invalid-feedback">
@@ -39,17 +58,32 @@ export function Entries(){
                             <div></div>
                             {visible?<Calendar onChange={onChange} value={value}/>:<div></div>}                            
                         </div>                        
-                        <div className="col-sm-6" class="w-50 p-3">
+                        <div className="col-sm-6 w-50 p-3">
                             <label className="form-label" >Entrada</label>
-                            <select className="form-label" class="form-control">
-                                <option defaultValue="entry">Entrada Simple</option>
-                                <option defaultValue="meeting">Cita</option>
+                            <select className="form-label form-control" 
+                            onChange={(e)=>setEntrada(e.currentTarget.value)}>
+                                <option value="entry">Entrada Simple</option>
+                                <option value="meeting">Cita</option>
                             </select>
                         </div>
                         <Lists typeL="worker" fieldL="fullname" 
                         esp="Trabajadores" listStP={listSt}/>
-                        <Lists typeL="service" fieldL="name"
-                        esp="Servicios" listStP={listSt}/>
+                        <div className="col-sm-6 w-50 p-3">
+                            <label htmlFor="precio" className="form-label">Precio</label>
+                            <input onChange={e=>handleChange(e)} id="precio" type="text" className="form-control" placeholder="" required/>
+                            <div className="invalid-feedback">
+                                Se requiere un precio valido.
+                            </div>
+                        </div>
+                        <div className="col-sm-6 w-50 p-3">
+                            <label htmlFor="manual" className="form-label">Servicio Manual</label>
+                            <input onChange={e=>handleChange(e)} id="manual" type="text" className="form-control" placeholder="" required/>
+                            <div className="invalid-feedback">
+                                Se requiere un Servicio valido.
+                            </div>
+                        </div>
+                        <div className="col-sm-6 w-100 p-3">-</div>
+                        <button type='submit' className="w-100 btn btn-primary btn-lg" >Crear</button>
                     </div>                    
                 </form>                
             </div>

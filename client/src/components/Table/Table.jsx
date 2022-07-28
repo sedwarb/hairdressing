cde1a7f411a3daa54068a7b66fe79e5e2c6d5995
sedwarb <sedwarb@gmail.com>
2022-07-27 23:05:43 -0500

    added props for draw three types of tables

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,24 +1,23 @@
 import React, { useState,useEffect } from 'react'
 
-export function Table(){
+export function Table({typeTable,search}){
     const [dates,setDates] = useState([]);
     useEffect(()=>{
-        fetch(`http://localhost:3001/worker`)
+        fetch(`http://localhost:3001/${search}`)
         .then(r=>r.json())
         .then(resp=>setDates(resp))
         .catch(error=>console.log(`Error: ${error}`)) 
-    },[])    
+    },[search])    
     return(
         <>
             <table className="table table-striped">
                 <thead>
                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nombre</th>
+                        {typeTable && typeTable.map(title=><th scope="col">{title.str}</th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    {dates && dates.map(date=><tr><td>{date.id}</td><td>{date.fullname}</td></tr> ) }
+                    {dates && dates.map(date=><tr>{typeTable && typeTable.map(type=><td>{date[type.name]}</td>)}</tr>)}
                 </tbody>
             </table>
         </>

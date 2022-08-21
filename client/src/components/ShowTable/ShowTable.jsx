77df486a1b391d991447d0ea8f8040d363f1bb71
sedwarb <sedwarb@gmail.com>
2022-08-20 22:20:55 -0500

    arregle unos errores que mostraba en la consola y otro error que no permitia mostrar la tabla de fechas

--- client/src/components/ShowTable/ShowTable.jsx
+++ client/src/components/ShowTable/ShowTable.jsx
@@ -44,74 +44,56 @@ export function ShowTable(){
                         <button disabled={table.service} id="service" className="btn btn-primary" onClick={e=>showTableNow(e)}>Servicios</button>
                     </li>
                     <li className="nav-item">
-                        <p>                            
-                            <button onClick={boton} className="btn btn-primary" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2 multiCollapseExample3 multiCollapseExample4">
+                        <p>
+                            <button onClick={boton} className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseEntradas" aria-expanded="false" aria-controls="collapseExample">
                                 Entradas
                             </button>
                         </p>
-                        <div className="row">
-                            <div className="col">
-                                <div className="collapse multi-collapse" id="multiCollapseExample1">
-                                    <div className="card card-body">
-                                        <p>                                            
-                                            <div className="input-group mb-3" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                                <div className="input-group-prepend">
-                                                    <span className="input-group-text" id="basic-addon3">F. Inicio</span>
-                                                </div>
-                                                <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" placeholder={table.dbeg}/>
-                                            </div>
-                                        </p>
-                                        <div className="collapse" id="collapseExample">
-                                            <div className="card card-body">
-                                                <Calendar onChange={onChange} value={table.cdbeg} />
-                                            </div>
+                        <div className="collapse" id="collapseEntradas">
+                            <div className="row">
+                                <div className="col">
+                                    <div className="input-group mb-3" data-toggle="collapse" data-target="#collapseCal1" aria-expanded="false" aria-controls="collapseExample">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="basic-addon3">F. Inicio</span>
+                                        </div>
+                                        <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" placeholder={table.dbeg} />
+                                    </div>
+                                    <div className="collapse" id="collapseCal1">
+                                        <div className="card card-body">
+                                            <Calendar onChange={onChange} value={table.cdbeg} />                                            
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col">
-                                <div className="collapse multi-collapse" id="multiCollapseExample2">
-                                    <div className="card card-body">
-                                        <p>
-                                            <div className="input-group mb-3" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
-                                                <div className="input-group-prepend">
-                                                    <span className="input-group-text" id="basic-addon3">F. Fin</span>
-                                                </div>
-                                                <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" placeholder={table.dend}/>
-                                            </div>
-                                        </p>
-                                        <div className="collapse" id="collapseExample2">
-                                            <div className="card card-body">
-                                                <Calendar onChange={onChangef} value={table.cdend} />
-                                            </div>
+                                <div className="col">
+                                    <div className="input-group mb-3" data-toggle="collapse" data-target="#collapseCal2" aria-expanded="false" aria-controls="collapseExample2">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="basic-addon3">F. Fin</span>
+                                        </div>
+                                        <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" placeholder={table.dend} />
+                                    </div>
+                                    <div className="collapse" id="collapseCal2">
+                                        <div className="card card-body">
+                                            <Calendar onChange={onChangef} value={table.cdend} />                                            
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col">
-                                <div className="collapse multi-collapse" id="multiCollapseExample3">
-                                    <div className="card card-body">
-                                        <div className="input-group mb-3">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text" id="basic-addon3">Tipo</span>
-                                            </div>
-                                            <select className="form-label form-control" onChange={(e) => setTable({ ...table, typed: e.currentTarget.value})}>
-                                                <option className="list-group-item">...Seleccione</option>
-                                                {tableFilter && tableFilter.map(date=><option className="list-group-item" key={date.str} value={date.str}>{date.name}</option>) }
-                                            </select>
+                                <div className="col">
+                                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="basic-addon3">Tipo</span>
                                         </div>
+                                        <select className="form-label form-control" onChange={(e) => setTable({ ...table, typed: e.currentTarget.value })}>
+                                            <option className="list-group-item">...Seleccione</option>
+                                            {tableFilter && tableFilter.map(date => <option className="list-group-item" key={date.str} value={date.str}>{date.name}</option>)}
+                                        </select>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col">
-                                <div className="collapse multi-collapse" id="multiCollapseExample4">
-                                    <div className="card card-body">
-                                        <div className="input-group mb-3">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text" id="basic-addon3">Filtro</span>
-                                            </div>
-                                            <input onChange={(e) => setTable({ ...table, tdate: e.target.value })} type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
+                                <div className="col">
+                                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="basic-addon3">Filtro</span>
                                         </div>
+                                        <input onChange={(e) => setTable({ ...table, tdate: e.target.value })} type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
                                     </div>
                                 </div>
                             </div>

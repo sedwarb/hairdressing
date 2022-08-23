cd5634e740120f3a106eb2efd246965111f4b632
sedwarb <sedwarb@gmail.com>
2022-08-22 21:45:00 -0500

    se creo un nuevo componente que muestra las tablas y se modificaron otros archivos

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import React from 'react'
 import { Route } from  "react-router-dom";
 import {Entries} from './components/entries/Entries'
 import {ShowTable} from './components/ShowTable/ShowTable'
+import {ShowMeTables} from './components/ShowMeTables/ShowMeTables'
 
 function App() {
   return (
     <React.Fragment>
       <Route exact path="/Entries" component={Entries}></Route>
-      <Route exact path="/Table" component={ShowTable}></Route>
+      <Route exact path="/Table" component={ShowMeTables}></Route>
     </React.Fragment>
   );
 }
---
+++ client/src/components/ShowMeTables/ShowMeTables.jsx
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import {Table} from '../Table/Table'
+import {typeTable,stateTable,cleanTableF,tableFilter} from "../constAndFunctions/constAndFunions.js"
+
+export function ShowMeTables(){
+    const [table,setTable] = useState(stateTable)
+    function handleChange(e){
+        setTable({...table,[e.target.id]:e.target.value})
+    }
+    return(
+        <>
+            <h2 class="text-center font-weight-bold my-3">Reporte por Rango de Fecha</h2>
+            <div className="row px-2 mt-2">
+                <div className="col">
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="fecha-inicio">F. Inicio</span>
+                        </div>
+                        <input onChange={(e)=>handleChange(e)} id='dbeg' type="date" className="form-control" aria-label="Username" aria-describedby="fecha-inicio" />
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="fecha-fin">F. Fin</span>
+                        </div>
+                        <input onChange={(e)=>handleChange(e)} id='dend' type="date" className="form-control" aria-label="Username" aria-describedby="fecha-fin" />
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="tipo">Tipo</span>
+                        </div>
+                        <select className="form-label form-control" onChange={(e) => setTable({ ...table, typed: e.currentTarget.value })}>
+                            <option className="list-group-item">...Seleccione</option>
+                            {tableFilter && tableFilter.map(date => <option className="list-group-item" key={date.str} value={date.str}>{date.name}</option>)}
+                        </select>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="filtro">Filtro</span>
+                        </div>
+                        <input id='tdate' onChange={(e) => handleChange(e)} type="text" className="form-control" aria-label="Username" aria-describedby="filtro" />
+                    </div>
+                </div>
+            </div>
+            <div className='row my-1'>
+                <button onClick={()=>setTable({...table,type:"",send:true})} className="mx-auto btn btn-primary btn-lg" >Enviar</button>
+            </div>
+            <div className='mt-3 px-2' >                
+                <Table typeTable={typeTable.entries} search={table}/>
+            </div>
+        </>
+    )
+
+}

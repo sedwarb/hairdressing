1aafd96ba93d6fcfaa11ba66667e9578d1cc5c3c
sedwarb <sedwarb@gmail.com>
2022-10-07 21:11:48 -0500

    se corrieron errores: si no se selecciona worker y service, no guarda. si se escribe un id y no se busca, no guarda

--- client/src/components/entries/Oentries.jsx
+++ client/src/components/entries/Oentries.jsx
@@ -15,7 +15,9 @@ const tabla_st = {
     amountEntry:null,    
     //no necesarios    
     date:"",    
-    entryType:"",    
+    entryType:"",
+    telephone:null,
+    nomUsu:null
 }
 
 //Este componente solo sera para entradas, las citas sera otro componente
@@ -41,25 +43,30 @@ export function Oentries(){
         css.mostrado?setCss({...css,mostrado:false}):setCss({...css,mostrado:true})
     }
     function guardar(){
-        if(entries.serviceId==="inma"?validar_inma():true){
-            let fecha = new Date()
-            setTabla([...tabla,
-            {
-                date: `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`,
-                manualEntry: entries.serviceId.valor==="inma"?entries.manualEntry:"No Aplica",
-                amountEntry: entries.serviceId.valor==="inma"?entries.amountEntry:"No Aplica",
-                user: entries.telephone?entries.nomUsu:"Usuario Generico",
-                worker: entries.workerId.nombre,
-                service: entries.serviceId.nombre
-            }
-            ])
-            setEntries({...entries,userName:""})
-            document.querySelector('#manualEntry').value=""
-            document.querySelector('#amountEntry').value=""
-            document.querySelector('#telephone').value=""
-            document.querySelector('#userName').value=""
-            onSubmit(entries)
-        }else alert("Debe llenar los campos manuales")
+        if(entries.telephone!==null ? entries.nomUsu===null ? false : true : true){
+            if(entries.serviceId==="inma"?validar_inma():true){
+                if(entries.workerId && entries.serviceId){                
+                    let fecha = new Date()
+                    setTabla([...tabla,
+                    {
+                        date: `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`,
+                        manualEntry: entries.serviceId.valor === "inma" ? entries.manualEntry : "No Aplica",
+                        amountEntry: entries.serviceId.valor === "inma" ? entries.amountEntry : "No Aplica",
+                        user: entries.telephone ? entries.nomUsu : "Usuario Generico",
+                        worker: entries.workerId.nombre,
+                        service: entries.serviceId.nombre
+                    }
+                    ])
+                    setEntries({ ...entries, userName: "" })
+                    document.querySelector('#manualEntry').value = ""
+                    document.querySelector('#amountEntry').value = ""
+                    document.querySelector('#telephone').value = ""
+                    document.querySelector('#userName').value = ""
+                    onSubmit(entries)
+                    setEntries({ ...entries, telephone: null,nomUsu:null })
+                }else alert("Debe completar los datos Servicio y Trabajador")
+            }else alert("Debe llenar los campos manuales")
+        }
     }
     return(
         <>
